7408030ee9c9e354cb90054b59d2d8fd11e2ac01
SatriaAPN <satrianusa10@gmail.com>
2022-01-16 22:13:52 +0700

    fix bug in authentication middleware

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.set('view engine', 'ejs');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(403).json({ error: 'No credentials sent!' });
   }
