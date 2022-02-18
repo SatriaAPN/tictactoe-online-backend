1ad659a7563c9377590040b312a84ec559cb165b
SatriaAPN <satrianusa10@gmail.com>
2022-02-18 16:44:40 +0700

    refactoring JWT function

---
+++ src/lib/jwtFunction.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const {
+  jwtSecretKey
+} = require('../config/config');
+
+const verifJwtToken = async(token) => {
+  const jwtToken = token.split(' ')[2]; // Header jwt {token}
+
+  const userData = await jwt.verify(jwtToken, jwtSecretKey);
+  userData.token = jwtToken;
+  
+  return userData;
+}
+
+const signJwtToken = async(username, uuid) => {
+  return await jwt.sign({ username, uuid }, jwtSecretKey);
+}
+
+module.exports = {verifJwtToken, signJwtToken};
