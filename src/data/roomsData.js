9d90cd8d1adc37bef772a902c4f54f060ae0827b
SatriaAPN <satrianusa10@gmail.com>
2022-02-18 16:44:05 +0700

    refactoring roomsData

---
+++ src/data/roomsData.js
@@ -0,0 +1,58 @@
+class roomsData {
+  constructor() {
+    if (typeof roomsData.INSTANCE === 'object') {
+      return roomsData.INSTANCE;
+    }
+    
+    this.rooms = {};
+    roomsData.INSTANCE = this;
+  }
+  
+  addRoom(roomData) {
+    this.rooms[roomData.roomUuid] = roomData;
+  }
+
+  getRoom(uuid) {
+    return this.rooms[uuid];
+  }
+
+  deleteRoom(roomUuid) {
+    delete this.rooms[roomUuid];
+  }
+
+  joinRoom(roomUuid, playerData) {
+    this.rooms[roomUuid].players.push(playerData);
+  }
+
+  findPlayer(roomUuid, playerUuid) {
+    // find the player that left in the room's players array
+    const playerIndex = this.rooms[roomUuid].players.map(as=>as.uuid).indexOf(playerUuid);
+
+    // check if the player exist in the room's players array
+    if(playerIndex === -1) throw new Error('player did not found');
+
+    return playerIndex;
+  }
+
+  leftRoom(roomUuid, playerUuid) {
+    const playerIndex = this.findPlayer(roomUuid, playerUuid);
+    // remove the player that left from the room's players array
+    this.rooms[roomUuid].players.splice(playerIndex, 1);
+  }
+
+  getAllRooms() {
+    return Object.values(this.rooms);
+  }
+
+  playerReady(roomUuid, playerUuid, playerReadyStatus) {
+    const playerIndex =  this.findPlayer(roomUuid, playerUuid);
+
+    this.rooms[roomUuid].players[playerIndex].ready = playerReadyStatus;
+  }
+
+  setRoomPlaying(roomUuid, roomStatus) {
+    this.rooms[roomUuid].roomPlaying = roomStatus;
+  }
+}
+
+module.exports = roomsData;
