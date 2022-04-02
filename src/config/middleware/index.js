7b396f17f3b44f2c26f8662b7f7e2d7ccb8a791e
SatriaAPN <satrianusa10@gmail.com>
2022-04-02 07:32:59 +0700

    update the middleware

--- src/config/middleware/index.js
+++ src/config/middleware/index.js
@@ -6,7 +6,9 @@ const {
 } = require('../config');
 
 app.use(cors());
+// parse application/json, basically parse incoming Request Object as a JSON Object 
 app.use(express.json());
+// parse application/x-www-form-urlencoded, basically can only parse incoming Request Object if strings or arrays
 app.use(express.urlencoded());
 app.use(require('./jwtMiddleware')); // jwt authentication
 
